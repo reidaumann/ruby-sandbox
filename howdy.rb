0561f22b8411f0334fe6860988d7152dfedccdcc
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2023-11-19 15:44:41 +0000

    added activesupport gem

--- howdy.rb
+++ howdy.rb
@@ -3,3 +3,11 @@ my_string="Hello, world!"
 pp my_string
 
 require "./goodbye.rb"
+require "active_support/all"
+
+1.ordinalize    # => "1st"
+2.ordinalize    # => "2nd"
+53.ordinalize   # => "53rd"
+2009.ordinalize # => "2009th"
+-21.ordinalize  # => "-21st"
+-134.ordinalize # => "-134th"
