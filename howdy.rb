e25ed6758c705492230d476b4c557cac68d7698f
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2023-11-19 15:48:59 +0000

    renamed Gemfile and imported two more gems

--- howdy.rb
+++ howdy.rb
@@ -5,9 +5,8 @@ pp my_string
 require "./goodbye.rb"
 require "active_support/all"
 
-pp 1.ordinalize    # => "1st"
-2.ordinalize    # => "2nd"
-53.ordinalize   # => "53rd"
-2009.ordinalize # => "2009th"
--21.ordinalize  # => "-21st"
--134.ordinalize # => "-134th"
+pp "What's your name?"
+
+their_name = gets
+
+pp "Hello, " + their_name + "!"
